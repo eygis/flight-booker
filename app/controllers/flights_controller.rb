90ae8e0a07e0f34300ac9360acc0c0cfbe9655a1
eygis <ahill033@yahoo.com>
2022-08-28 01:57:51 +0900

    slight refactoring

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,9 @@
 class FlightsController < ApplicationController
   def index
+    @flight = params[:flight] ? Flight.new(search_params) : Flight.new
     @airports = Airport.all.map{|a| [a.airport_city, a.id]}
     @flights = Flight.all
-    @search = params[:search] ? search_params.select{|k, v| v != ''} : nil
+    @search = params[:flight] ? search_params.select{|k, v| v != ''} : nil
     if @search
       @results = !@search.empty? ? Flight.where(@search).includes(:departure_airport, :arrival_airport) : @flights
     end
@@ -11,6 +12,6 @@ class FlightsController < ApplicationController
   private
 
   def search_params
-    params.require(:search).permit(:departure_airport_id, :arrival_airport_id, :flight_date)
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :flight_date)
   end
 end
